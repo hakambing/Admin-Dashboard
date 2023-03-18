6f27fd2f70979221616f537a488f655e5110f5a0
hakambing <abdulhakamhasbullah@gmail.com>
2023-03-18 19:06:54 +0800

    Fixed menu bar

---
+++ script.js
@@ -0,0 +1,9 @@
+// var menu = document.getElementById("menu");
+// var sideBar = document.querySelector('.side-menu');
+
+// if (menu.checked==true){
+//     sideBar.style.display = "block";
+// } else {
+//     sideBar.style.display = "none";
+// }
+
